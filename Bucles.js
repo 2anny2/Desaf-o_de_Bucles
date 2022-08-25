870c5ad6516fbd8601ff1c3b46855b431e5523f8
2anny2 <anny192922@gmail.com>
2022-08-25 10:51:03 -0400

    first commit

---
+++ Bucles.js
@@ -0,0 +1,33 @@
+// -Imprimir Pares 1-20 //
+var i =1;
+for( var i = 1; i < 20; i=+2){
+    console.log(i);
+}
+
+// -Disminuir múltiplos de 3 //
+var i =100;
+for( var i=100; i>-1 ; i--){
+    if(i % 3==0){
+        console.log(i);
+    }
+}
+
+// -Imprimir la Secuencia//
+var i =4;
+for(var i=4; i>-4; i=-1.5){
+    console.log(i);
+}
+
+// -Suma//
+var Suma =0;
+for(var i=1; i<101; i++){
+    Suma+=i;
+}
+console.log(Suma);
+
+// -Producto//
+var Producto =1;
+for(var i=1; i<13; i++){
+    Producto*=i;
+}
+console.log(Producto);
